2458c30b7f2e951a1c7bb4c56da7d6f5d3c476ea
nick <nick>
2016-03-31 00:08:14 -0400

    file name change

--- app/src/main/java/edu/fau/ngamarra2014/sync_care/HomeActivity.java
+++ app/src/main/java/edu/fau/ngamarra2014/sync_care/HomeActivity.java
@@ -6,10 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import org.json.JSONArray;
