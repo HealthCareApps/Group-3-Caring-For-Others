bba3960a4eec606d5514501869d142e29e21846a
Nicholas A <nicholasangiolilli@gmail.com>
2016-04-18 21:43:59 -0400

    Implemented 4 activities in my account

--- Sync-Care Project/app/src/main/java/edu/fau/ngamarra2014/sync_care/ManageProfileActivity.java
+++ Sync-Care Project/app/src/main/java/edu/fau/ngamarra2014/sync_care/ManageProfileActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
-public class DeletePatientActivity extends Activity {
+public class ManageProfileActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delete_patient);
+        setContentView(R.layout.activity_manage_profile);
     }
+
 }
